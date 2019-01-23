b38a57feae80c690560aafa07d02d3f7eb99aed1
jCzachor6 <jakubczachor7@gmail.com>
2019-01-23 23:52:32 +0100

    Cleanup code 2

--- gesture.py
+++ gesture.py
@@ -5,7 +5,6 @@ import json
 import time
 import shortcuts as sc
 
-
 message_counter = 0
 counter = 0
 values = [0, 0, 0]
@@ -47,14 +46,28 @@ def json_message(message_counter, message_type):
 
 cap = cv2.VideoCapture(0)
 while (cap.isOpened()):
+    #Przechwyć obraz do zmiennej img
     ret, img = cap.read()
+    #Rysuj kwadrat na obrazie img
     cv2.rectangle(img, (300, 300), (100, 100), (0, 255, 0), 0)
+    #Wycięcie potrzebnego fragmentu obrazu
     crop_img = img[100:300, 100:300]
+
+    #1. Przerobienie wyciętego obrazu na o odcieniach szarości
     grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+
+    #2. Rozmycie obrazu szarego
     value = (35, 35)
     blurred = cv2.GaussianBlur(grey, value, 0)
+
+    #3. Zastosowanie binaryzacji + Metody OTSU
     _, thresh1 = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    #4. Konturowanie obrazu binarnego
     _, contours, hierarchy = cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    #5. Wyznaczenie najbardziej odległych konturów w obrazie binarnym i obrysowanie kwadratu na podstawie najbardziej
+    #skrajnych punktów dłoni
     max_area = -1
     for i in range(len(contours)):
         cnt = contours[i]
@@ -65,14 +78,20 @@ while (cap.isOpened()):
     cnt = contours[ci]
     x, y, w, h = cv2.boundingRect(cnt)
     cv2.rectangle(crop_img, (x, y), (x + w, y + h), (0, 0, 255), 0)
+
+    #Obrysowanie dłoni otoczką wypukłą
     hull = cv2.convexHull(cnt)
     drawing = np.zeros(crop_img.shape, np.uint8)
     cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 0)
     cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 0)
+
+    #Znalezienie skrajnych punktów z wylicznonej powierzchni między dłonią a otoczką wypukłą dłoni.
     hull = cv2.convexHull(cnt, returnPoints=False)
     defects = cv2.convexityDefects(cnt, hull)
-    current_value = 0
     cv2.drawContours(thresh1, contours, -1, (0, 255, 0), 3)
+
+    # Przeliczenie tych defektów których kąty między sąsiednimi punktami są mniejsze od 90 stopni. (Punkty wewnętrzne)
+    current_value = 0
     if(defects is not None):
         for i in range(defects.shape[0]):
             s, e, f, d = defects[i, 0]
@@ -88,6 +107,7 @@ while (cap.isOpened()):
                 cv2.circle(crop_img, far, 1, [0, 0, 255], -1)
             cv2.line(crop_img, start, end, [0, 255, 0], 2)
 
+    #Nalicz kolejną wartość licznika counter
     counter = count(counter, current_value)
 
     #Odczytaj gest
@@ -98,6 +118,9 @@ while (cap.isOpened()):
     #Złóż rysunek 'drawing' i 'crop_img' w jeden i wyświetl
     all_img = np.hstack((drawing, crop_img))
     cv2.imshow('Contours', all_img)
+    #Złóż rysunek 'Gray' i 'Blurred' w jeden i wyświetl
+    all_img_gray = np.hstack((grey, blurred))
+    cv2.imshow('Gray and blurred', all_img_gray)
 
     if quit_on_esc():
         break
